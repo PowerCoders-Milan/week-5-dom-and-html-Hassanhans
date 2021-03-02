af6b0fc059714e88ba34512a8da424b0fca9954f
hassanwasswa <assanwasswa@gmail.com>
2021-03-02 12:52:53 +0100

    execrsie2 done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,13 @@
-function getAttributes() {
 
+function getAttributes() {
+ var a = document.getElementById("pwcdr").href;
+ alert('The value of the href attribute of the link is : '+a);
+ var b = document.getElementById("pwcdr").hreflang;   
+ alert('The value of the hreflang attribute of the link is : '+b);
+ var c = document.getElementById("pwcdr").rel; 
+  alert('The value of the rel attribute of the link is : '+c);
+ var d = document.getElementById("pwcdr").target; 
+  alert('The value of the taget attribute of the link is : '+d);
+ var e = document.getElementById("pwcdr").type; 
+  alert('The value of the type attribute of the link is : '+e);  
 }

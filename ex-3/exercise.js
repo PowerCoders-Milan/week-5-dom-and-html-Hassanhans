d5f87f1e17153371f309015fb15faa00768a2e30
hassanwasswa <assanwasswa@gmail.com>
2021-03-02 12:53:53 +0100

    execrsie3 done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,15 @@
-function insert_Row() {
 
+function insert_Row(){
+var a=document.getElementById('sampleTable').insertRow(0);
+var b = a.insertCell(0);
+var c = a.insertCell(1);
+b.innerHTML="New Cell1";
+c.innerHTML="New Cell2";
 }
+
+
+
+/*The innerHTML property is part of the Document Object Model (DOM) 
+that allows Javascript code to manipulate a website being displayed. 
+Specifically, it allows reading and replacing everything within 
+a given DOM element (HTML tag).*/
